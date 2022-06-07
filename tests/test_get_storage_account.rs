908ac0d8132beeaa7abe88e9bb3a19caa33fad47
dboures <deanboures@gmail.com>
2022-06-07 11:33:21 -0500

    test: test_get_multiple_storage_accounts passes

--- tests/test_get_storage_account.rs
+++ tests/test_get_storage_account.rs
@@ -1,7 +1,7 @@
-use solana_client::rpc_response::RpcKeyedAccount;
 use serde_json::json;
 use shadow_drive_rust::Client;
 use solana_account_decoder::{UiAccount, UiAccountEncoding};
+use solana_client::rpc_response::RpcKeyedAccount;
 use solana_client::{
     rpc_client::RpcClient,
     rpc_request::RpcRequest,
@@ -76,60 +76,61 @@ async fn test_get_storage_account() {
     assert_eq!(storage_account.identifier, "first-test");
 }
 
+#[tokio::test]
+async fn test_get_multiple_storage_accounts() {
+    let keypair = Keypair::new();
+    let storage_account_key_1 = Pubkey::new_unique();
+    let storage_account_key_2 = Pubkey::new_unique();
+    let owner_key = Pubkey::new_unique();
 
-// #[tokio::test]
-// async fn test_get_multiple_storage_accounts() {
-//     let keypair = Keypair::new();
-//     let storage_account_key_1 = Pubkey::new_unique();
-//     let storage_account_key_2 = Pubkey::new_unique();
-//     let owner_key = Pubkey::new_unique();
+    // Prepare mocks
+    let mut mocks = HashMap::new();
 
-//     // Prepare mocks
-//     let mut mocks = HashMap::new();
+    let mock_storage_account_1 = test_utilities::mock_storage_account_1();
+    let mock_storage_account_2 = test_utilities::mock_storage_account_2();
 
-//     let mock_storage_account_1 = test_utilities::mock_storage_account_1();
-//     let mock_storage_account_2 = test_utilities::mock_storage_account_2();
+    let encoded_storage_1 = UiAccount::encode(
+        &storage_account_key_1,
+        &mock_storage_account_1,
+        UiAccountEncoding::JsonParsed,
+        None,
+        None,
+    );
 
-//     let encoded_storage_1 = UiAccount::encode(
-//         &storage_account_key_1,
-//         &mock_storage_account_1,
-//         UiAccountEncoding::JsonParsed,
-//         None,
-//         None,
-//     );
+    let encoded_storage_2 = UiAccount::encode(
+        &storage_account_key_2,
+        &mock_storage_account_2,
+        UiAccountEncoding::JsonParsed,
+        None,
+        None,
+    );
 
-//     let encoded_storage_2 = UiAccount::encode(
-//         &storage_account_key_2,
-//         &mock_storage_account_2,
-//         UiAccountEncoding::JsonParsed,
-//         None,
-//         None,
-//     );
+    let gpa_response = json!(vec![
+        RpcKeyedAccount {
+            pubkey: storage_account_key_1.to_string(),
+            account: encoded_storage_1
+        },
+        RpcKeyedAccount {
+            pubkey: storage_account_key_2.to_string(),
+            account: encoded_storage_2
+        }
+    ]);
+    mocks.insert(RpcRequest::GetProgramAccounts, gpa_response);
 
-//     let gpa_response = json!(Response {
-//         context: RpcResponseContext { slot: 1 },
-//         value: //vec![
-//             RpcKeyedAccount {
-//                 pubkey: storage_account_key_1.to_string(),
-//                 account: encoded_storage_1
-//             },
-//             // RpcKeyedAccount {
-//             //     pubkey: storage_account_key_2.to_string(),
-//             //     account: encoded_storage_2
-//             // }
-//         //],
-//     });
-//     mocks.insert(RpcRequest::GetProgramAccounts, gpa_response);
+    // Create RPC + Client
+    let mock_rpc = RpcClient::new_mock_with_mocks("https://ssc-dao.genesysgo.net", mocks);
+    let shdw_drive_client = Client::new(keypair, mock_rpc);
 
-//     // Create RPC + Client
-//     let mock_rpc = RpcClient::new_mock_with_mocks("https://ssc-dao.genesysgo.net", mocks);
-//     let shdw_drive_client = Client::new(keypair, mock_rpc);
+    // get account
+    let storage_accounts = shdw_drive_client
+        .get_storage_accounts(&owner_key)
+        .await
+        .expect("failed to get storage account");
 
-//     // get account
-//     let storage_accounts = shdw_drive_client
-//         .get_storage_accounts(&owner_key)
-//         .await
-//         .expect("failed to get storage account");
-
-//     assert_eq!(storage_accounts.len(), 2);
-// }
+    assert_eq!(storage_accounts.len(), 2);
+    assert_eq!(storage_accounts[0].identifier, "first-test");
+    assert_eq!(storage_accounts[0].account_counter_seed, 19);
+    assert_eq!(storage_accounts[0].identifier, "first-test");
+    assert_eq!(storage_accounts[1].identifier, "second-test");
+    assert_eq!(storage_accounts[1].account_counter_seed, 7);
+}
