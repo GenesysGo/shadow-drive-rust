9bd5d0fef4b5999e73bd798b6956ca6fb1313f04
Tracy Adams <31976538+tracy-codes@users.noreply.github.com>
2023-03-15 11:17:21 -0700

    Revert "add runes crate"
    This reverts commit b4ff28e9116ab3182b75b50ba13a4595c374046d.

--- runes/examples/use_cli_runes.rs
+++
@@ -1,8 +0,0 @@
-use runes::{inscribe_runes, Rune, Runes};
-
-inscribe_runes!("../../pots.runes");
-
-fn main() {
-    let runes = unsafe { get_runes() };
-    println!("{runes:?}");
-}
--- runes/src/inscribe.rs
+++
@@ -1,9 +0,0 @@
-#[macro_export]
-macro_rules! inscribe_runes {
-    ($path:literal) => {
-        static __PRIVATE_INNER_RUNES_DATA: &'static [u8] = include_bytes!($path);
-        pub unsafe fn get_runes() -> &'static runes::ArchivedRunes {
-            rkyv::archived_root::<runes::Runes>(__PRIVATE_INNER_RUNES_DATA)
-        }
-    };
-}
