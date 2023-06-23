7412f844f82bfc075c958fff3970bb042a2798d7
cavemanloverboy <caveycool@gmail.com>
2023-06-23 12:22:23 -0700

    add withdraw command

--- cli/src/command/nft/collection/mod.rs
+++ cli/src/command/nft/collection/mod.rs
@@ -3,11 +3,13 @@ use shadow_drive_sdk::{Pubkey, Signer};
 
 mod get;
 mod init;
+mod withdraw;
 
 #[derive(Debug, Parser)]
 pub enum CollectionCommand {
     Init,
     Get { collection: Pubkey },
+    Withdraw { collection: Pubkey },
 }
 impl CollectionCommand {
     pub async fn process(&self, signer: &impl Signer, rpc_url: &str) -> anyhow::Result<()> {
@@ -15,6 +17,9 @@ impl CollectionCommand {
             CollectionCommand::Init => init::process(signer, rpc_url).await,
 
             CollectionCommand::Get { collection } => get::process(collection, rpc_url).await,
+            CollectionCommand::Withdraw { collection } => {
+                withdraw::process(signer, *collection, rpc_url).await
+            }
         }
     }
 }
